4f9f2b15e9f7c889a026d6af4d3ac067bf53fa63
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-12-25 00:02:21 +0300

    test

--- src/test/java/week1/FirstSeleniumClass.java
+++ src/test/java/week1/FirstSeleniumClass.java
@@ -1,2 +1,24 @@
-package week1;public class FirstSeleniumClass {
+package week1;
+
+import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.edge.*;
+import org.openqa.selenium.firefox.*;
+
+public class FirstSeleniumClass {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.edgedriver().setup();
+
+        WebDriver driverChrome = new ChromeDriver();
+
+        driverChrome.get("https://practice.cydeo.com/");
+
+        WebDriver driverEdge = new EdgeDriver();
+
+        driverEdge.get("https://www.google.com");
+    }
 }
